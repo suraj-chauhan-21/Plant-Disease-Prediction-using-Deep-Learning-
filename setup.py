ebe721bfee0516a048cadae83f4a40c543ca76fa
Suraj Chauhan <surajc21@iiserb.ac.in>
2026-06-08 22:16:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="plant_disease_detection",
+    version="1.0.0",
+    author="Suraj Chauhan",
+    description="A high-performance plant disease detection system using deep learning.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/suraj/plant-disease-detection",
+    packages=find_packages(include=["src", "src.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.8",
+)
